4d67bdf4ed820e533f7bb09ae831b8dad85a4779
justrunme <justrunme@gmail.com>
2025-06-28 22:50:36 +0200

    Use treadofkhan/prowler image from Docker Hub

--- scripts/run_all_audits.sh
+++ scripts/run_all_audits.sh
@@ -78,7 +78,7 @@ echo "aws_secret_access_key = wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY" >> ~/.aw
 docker run -t --rm \
   -v ~/.aws:/root/.aws \
   -v "$(pwd)"/reports/prowler:/prowler/output \
-  ghcr.io/prowler-cloud/prowler:latest \
+  treadofkhan/prowler \
   -M html,csv,json -S -n
 
 echo "Prowler finished. Reports saved to reports/prowler/"
